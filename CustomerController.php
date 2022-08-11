c95b12a487cda976fed1258e84adfb97a60ae091
prorakeshraut <35736338+prorakeshraut@users.noreply.github.com>
2022-08-11 17:27:45 +0545

    Update CustomerController.php

--- CustomerController.php
+++ CustomerController.php
@@ -11,7 +11,10 @@ class CustomerController extends Controller
 {
     public function index()
     {
-        return view('customer');
+        $url = url('/customer');
+        $title = "Customer Registration";
+        $data = compact('url','title');
+        return view('customer')->with($data);
     }
     
     public function store(Request $request)
@@ -41,4 +44,44 @@ class CustomerController extends Controller
         $data = compact('customers');
         return view('customer-view')->with($data);
     }
+
+    public function delete($id)
+    {
+        $customer = Customer::find($id);
+        if(!is_null($customer)){
+            $customer->delete();
+        }
+        return redirect()->back();
+        
+    }
+
+    public function edit($id)
+    {
+       $customer = Customer::find($id);
+       if(is_null($customer)){
+        return redirect('customer/view');
+       }else{
+            $url = url('/customer/update') . "/" . $id;
+            $title = "Update Customer";
+            $data = compact('customer','url','title');
+            return view('customer')->with($data);
+       }
+    }
+
+    public function update($id, Request $request)
+    {
+        $customer = Customer::find($id);
+        $customer->name = $request['name'];
+        $customer->email = $request['email'];
+        $customer->gender = $request['gender'];
+        $customer->address = $request['address'];
+        $customer->state = $request['state'];
+        $customer->country = $request['country'];
+        $customer->dob = ($request['dob']);
+        $customer->password = md5($request['password']);
+        $customer->confirm_password = md5($request['confirm_password']);
+        $customer->save();
+        return redirect('customer/view');
+    }
 }
+
